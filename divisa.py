00c721b725bd1052786fecc148b05a78f11d5a80
Daniel <danielahumada071@gmail.com>
2023-07-31 11:05:17 -0500

    NEW: now we can see an error message when the currency does'nt exists
    note: additional. i add a ---- when the result appears

--- divisa.py
+++ divisa.py
@@ -1,9 +1,14 @@
 from exchange import exchange
 
-currency_in = input("Please, give me the orginal currency \n for example USD, MXN, COP \n ->")
-currency_out = input("now, give the currency what you wanna get \n ->")
+currency_in = input("Please, give me the orginal currency \n -> ")
+currency_out = input("now, give the currency what you wanna get \n -> ")
 money = float(input("money: "))
 
 total = exchange(currency_in.upper(), currency_out.upper(), money)
 
-print(f"that's equal to {total} {currency_out.upper()}")
+print("-" * 50)
+
+if total == False:
+    print("error whit the currency")
+else:
+    print(f"that's equal to {total} {currency_out.upper()}")
