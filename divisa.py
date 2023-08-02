7b6f29617e3700ebb44007e06f41765cfde34168
Daniel <danielahumada071@gmail.com>
2023-08-02 11:35:00 -0500

    NEW feature: the pogram can support many exchanges

--- divisa.py
+++ divisa.py
@@ -7,14 +7,23 @@ def message(total, value):
         print("error whit the currency")
     else:
         print(f"that's equal to {total} {value.upper()}")
+    
+    print("-" * 50)
 
 def main():
-    currency_in = input("Please, give me the orginal currency \n -> ")
-    currency_out = input("now, give the currency what you wanna get \n -> ")
-    money = float(input("money: "))
+    while True:
+        currency_in = input("Please, give me the orginal currency \n -> ")
+        currency_out = input("now, give the currency what you wanna get \n -> ")
+        money = float(input("money: "))
 
-    total = exchange(currency_in.upper(), currency_out.upper(), money)
+        total = exchange(currency_in.upper(), currency_out.upper(), money)
 
-    message(total, currency_out)
+        message(total, currency_out)
+
+        exit = input("you want to do any other transaction? \ny: yes / n: ")
+        if exit == "n":
+            break
+
+        print("/" * 50)
 
 main()
