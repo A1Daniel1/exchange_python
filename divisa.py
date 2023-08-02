da0ce62e47c224e154420802f89b56911c3369b2
Daniel <danielahumada071@gmail.com>
2023-08-02 11:18:35 -0500

    organize the divisa.py in functions

--- divisa.py
+++ divisa.py
@@ -1,14 +1,20 @@
 from exchange import exchange
 
-currency_in = input("Please, give me the orginal currency \n -> ")
-currency_out = input("now, give the currency what you wanna get \n -> ")
-money = float(input("money: "))
+def message(total, value):
+    print("-" * 50)
 
-total = exchange(currency_in.upper(), currency_out.upper(), money)
+    if total == False:
+        print("error whit the currency")
+    else:
+        print(f"that's equal to {total} {value.upper()}")
 
-print("-" * 50)
+def main():
+    currency_in = input("Please, give me the orginal currency \n -> ")
+    currency_out = input("now, give the currency what you wanna get \n -> ")
+    money = float(input("money: "))
 
-if total == False:
-    print("error whit the currency")
-else:
-    print(f"that's equal to {total} {currency_out.upper()}")
+    total = exchange(currency_in.upper(), currency_out.upper(), money)
+
+    message(total, currency_out)
+
+main()
